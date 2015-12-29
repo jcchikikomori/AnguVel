36f1e72478e07bfe77e3a4b560004bfc87f064fc
jccultima123 <johncyrillcorsanes@gmail.com>
2015-12-29 16:19:13 +0800

    [FIX] Some fixes in UI

--- api/config/view.php
+++ api/config/view.php
@@ -15,7 +15,7 @@ return [
 
     'paths' => [
         // realpath(base_path('resources/views')),
-        realpath(base_path('../public/views')),
+        realpath(base_path('../public/app/views')),
     ],
 
     /*
